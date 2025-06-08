98328094484927028cf661d2538a6af0ba50e012
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2025-06-08 14:39:34 +0800

    [Style] update transparent of navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -65,7 +65,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="sticky max-h-16 top-0 z-50 w-full border-b bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/10 bg-opacity-30">
+    <nav className="sticky max-h-16 top-0 z-50 w-full border-b bg-white/95 backdrop-blur supports-[backdrop-filter]:bg-white/60">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
